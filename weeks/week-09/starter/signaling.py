af8343282a3253f0f9e6e5f5d83a54fe4b4ceadc
Roma-jpg1 <rpetrov2006@mail.ru>
2026-04-01 12:49:44 +0700

    week-09

--- weeks/week-09/starter/signaling.py
+++ weeks/week-09/starter/signaling.py
@@ -18,7 +18,7 @@ async def handler(websocket):
         CONNECTIONS.remove(websocket)
 
 async def main():
-    async with websockets.serve(handler, "localhost", 8765):
+    async with websockets.serve(handler, "localhost", 8237):
         await asyncio.Future()  # run forever
 
 if __name__ == "__main__":
